89f48f967335c0a293a58137c1069d6e7e984d96
marlie.dueck <marlie.dueck@sd27.bc.ca>
2020-11-19 12:51:30 -0800

    Commit Microsoft Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', 'HomeController@welcome');
+Route::get('/', [HomeController::class, 'welcome']);
+
+
+// Authentication routes
+Route::get('/signin', [AuthController::class, 'signin']);
+Route::get('/callback', [AuthController::class, 'callback']);
+Route::get('/signout', [AuthController::class, 'signout']);
